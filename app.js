efb14122fd366a10df098e66107441f9027a99c8
Gurvir Gill <gurvir@Gurvirs-MacBook-Pro.local>
2017-05-11 02:25:22 -0700

    multiple fixes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var session = require('client-sessions');
 
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
+var error = '';
 
 var User = mongoose.model('User', new Schema({
 	id: ObjectId,
@@ -20,13 +21,13 @@ var app = express();
 app.set('view engine', 'ejs');
 app.locals.pretty = true;
 
-
 // connect to mongo
 mongoose.connect('mongodb://Gurvir:hello1@ds133271.mlab.com:33271/us-db');
 
 //middleware
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(session({
 	cookieName: 'session',
@@ -36,23 +37,39 @@ app.use(session({
 }));
 
 app.get('/', function (req, res) {
-	res.render('index');
-	error = '';
+	res.locals.error = error;
+	if(req.session && req.session.user){
+		User.findOne( { email: req.session.user.email }, function(err, user){
+		if(!user){
+			req.session.reset();
+			res.redirect('/login');
+		} else {
+			res.locals.user = user;
+			res.render('index');
+			error = '';
+		}
+	});
+	} else{
+		res.locals.user = null;
+		res.render('index');
+		error = '';
+	}
 });
 
 app.get('/register', function(req,res) {
+	res.locals.error = error;
 	res.render('register');
 	error = '';
 });
 
 app.post('/register', function(req,res) {
+	res.locals.error = error;
 	var user = new User({
 		company: req.body.company,
 		job: req.body.job,
 		username: req.body.username,
 		email: req.body.email,
 		password: req.body.password,
-		error: "",
 	});
 	user.save(function(err) {
 		if(err){
@@ -82,11 +99,13 @@ app.post('/register', function(req,res) {
 });
 
 app.get('/login', function(req,res) {
+	res.locals.error = error;
 	res.render('login');
 	error = '';
 });
 
 app.post('/login', function(req,res) {
+	res.locals.error = error;
 	User.findOne( {email: req.body.email} , function(err, user) {
     	if(!user){
     		error: 'Invalid email or password';
@@ -104,6 +123,7 @@ app.post('/login', function(req,res) {
 });
 
 app.get('/profile', function(req,res) {
+	res.locals.error = error;
 	if(req.session && req.session.user){
 		User.findOne( { email: req.session.user.email }, function(err, user){
 			if(!user){
@@ -121,19 +141,36 @@ app.get('/profile', function(req,res) {
 });
 
 app.get('/logout', function(req, res) {
+	res.locals.error = error;
 	req.session.reset();
 	error = 'You have sucessfully logged out!';
 	res.redirect('/');
 });
 
 app.get('/edit_profile', function(req,res){
+	res.locals.error = error;
 	res.render('edit_profile');
 	error = '';
 });
 
 app.get('/change_picture', function(req, res){
+	res.locals.error = error;
 	res.render('change_picture');
 	error = '';
-
 });
+
+app.get('/announcement', function(req,res){
+	res.locals.error = error;
+	User.findOne( { email: req.session.user.email }, function(err, user){
+		if(!user){
+			req.session.reset();
+			res.redirect('/login');
+		} else {
+			res.locals.user = user;
+			res.render('announcement');
+			error = '';
+		}
+	});
+});
+
 app.listen(3000);
